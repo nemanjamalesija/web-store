823b1ac9651d6304631876a05e34b79bf0911c85
nemanjamalesija <malesija13@gmail.com>
2023-05-08 15:30:48 +0200

    install dependencies

--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,10 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(cors());
 app.use(express.json());

a693d5b63f48aca0e45dfc00aac48b1e13fde9f9
nemanjamalesija <malesija13@gmail.com>
2023-05-09 18:56:40 +0200

    Add first item

--- server/controllers/productsController.ts
+++ server/controllers/productsController.ts
@@ -1,6 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
 
-const getAllProducts = (req: Request, res: Response, next: NextFunction) => {
+const getAllProducts = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     res.status(200).json({
       status: 'sucess',
---
+++ server/models/propertyModel.ts
@@ -0,0 +1,3 @@
+import mongoose from 'mongoose';
+
+const propertySchema = new mongoose.Schema({});

433dce5445cd66196acb6a2fa3ec5a8dede64631
nemanjamalesija <malesija13@gmail.com>
2023-05-10 03:17:46 +0200

    Add server static middleware

--- server/app.ts
+++ server/app.ts
@@ -1,14 +1,20 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import { fileURLToPath } from 'url';
+import path from 'path';
 import productsRouter from './routes/productsRouter.ts';
 
 const app = express();
+
+const __fileName = fileURLToPath(import.meta.url);
+const __dirName = path.dirname(__fileName);
 
 // MIDDLEWARE
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
+app.use('/images', express.static(path.join(__dirName, 'dev-data/images')));
 
 // ROUTES
 app.use('/api/v1/products', productsRouter);

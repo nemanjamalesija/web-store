f8da946a94efe00656b26aaab561899e33021dc5
nemanjamalesija <malesija13@gmail.com>
2023-06-20 03:30:08 +0200

    Add signup page. Finish navigation

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,10 +7,15 @@ import { RouterLink, RouterView } from 'vue-router'
     <nav
       class="h-full px-10 flex items-center justify-between text-base lg:text-lg text-orange-50 font-medium border-b-2 text-inherit"
     >
-      <h1 class="text-xxl lg:text-2xl uppercase text-center font-semibold">Mambo jumbo bowls</h1>
+      <h1 class="text-xl lg:text-xl uppercase text-center font-medium">Mambo jumbo bowls</h1>
       <div class="flex items-center gap-6">
         <RouterLink to="/"> Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/login">Log in</RouterLink>
+        <RouterLink
+          to="/signup"
+          class="signup border border-orange-50 hover:bg-orange-50 hover:text-orange-950 py-2 px-6 rounded-full transition-all duration-300 hover:-translate-y-[3px]"
+          >Sign up</RouterLink
+        >
       </div>
     </nav>
   </header>
@@ -18,4 +23,7 @@ import { RouterLink, RouterView } from 'vue-router'
   <RouterView />
 </template>
 
-<style scoped></style>
+<style scoped>
+.signup {
+}
+</style>
---
+++ client/src/views/SignupView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="signup">
+    <h1>This will be the signup page</h1>
+  </div>
+</template>
+
+<style></style>

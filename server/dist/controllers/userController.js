2046f693e9d882290dffa3a30af6818ae65b424c
nemanjamalesija <malesija13@gmail.com>
2023-07-14 23:05:52 +0200

    Resolve multer photo upload

--- server/dist/controllers/userController.js
+++ server/dist/controllers/userController.js
@@ -63,15 +63,16 @@ const updateMe = catchAsync((req, res, next) =>
     });
   })
 );
-const multerStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'public/images/users');
-  },
-  filename: (req, file, cb) => {
-    const ext = file.mimetype.split('/')[1];
-    cb(null, `user-${Date.now()}.${ext}`);
-  },
-});
+// const multerStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'public/images/users');
+//   },
+//   filename: (req, file, cb) => {
+//     const ext = file.mimetype.split('/')[1];
+//     cb(null, `user-${Date.now()}.${ext}`);
+//   },
+// });
+const multerStorage = multer.memoryStorage();
 const multerFilter = (req, file, cb) => {
   if (file.mimetype.startsWith('image')) {
     cb(null, true);
